8021ac4b42e69787b0f5c3fd0ec6eda881390a28
tmd-uk <admin@thinkmilkdesign.co.uk>
2019-09-27 10:07:28 +0100

    Added new Contact component

---
+++ components/Contact.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="uk-section">
+    <div class="uk-container uk-flex-container">
+      <div class="uk-text-center">
+        <h1>CONTACT US</h1>
+      </div>
+    </div>
+  </div>
+</template>
