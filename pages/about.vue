3507ebd97d0539dcf70e9aa9caec081a54a4f3f5
tmd-uk <admin@thinkmilkdesign.co.uk>
2019-09-27 13:05:55 +0100

    Added about us to About page

--- pages/about.vue
+++ pages/about.vue
@@ -3,19 +3,12 @@
     <div class="uk-container uk-flex-container">
       <div class="uk-text-center">
         <h1>ABOUT US</h1>
-
-        <div class="uk-grid-match uk-child-width-1-3@m" uk-grid>
-          <div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
-          </div>
-          <div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
-          </div>
-          <div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
-          </div>
-        </div>
-
+        <section>
+          <p>Matt’s Car Care is a professional detailing service in Toowoomba, it’s our passion and we thrive on ensuring your vehicle gets the best detail around. We have grown significantly over the last few years, going from strength to strength.</p>
+          <p>Our detailing services cover most vehicles from cars to Utes, 4x4’s to semi-trucks. We find that by hand washing and hand cleaning, it gives the best detailing results which most automated detailers cannot achieve.</p>
+          <p>We take pride in the work we provide for your pride and joy, get in touch with us to find out how we get your pride and joy to showroom quality.</p>
+        </section>
+        <booking/>
       </div>
     </div>
     <clients />
@@ -24,10 +17,12 @@
 
 <script>
 import Clients from "~/components/Clients.vue";
+import Booking from "~/components/Booking.vue";
 
 export default {
   components: {
-    Clients
+    Clients,
+    Booking
   },
   head() {
     return {
