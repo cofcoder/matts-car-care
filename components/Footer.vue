730c0eaf6d2182d74e76c40e814523cd8ed9205c
tmd-uk <admin@thinkmilkdesign.co.uk>
2019-10-02 13:12:55 +0100

    Added After Pay logo below h3

--- components/Footer.vue
+++ components/Footer.vue
@@ -6,6 +6,7 @@
         <img src="~/assets/img/matts-car-care.svg" alt="Matt's Car Care">
       </nuxt-link> -->
       <h3>CALL US NOW ON <a href="tel:0402256181" class="uk-link-heading">0402 256 181</a></h3>
+      <img src="~/assets/img/afterpay.svg" width="300" height="85" alt="We accept AfterPay" title="We accept AfterPay">
       <p>© MATT'S CAR CARE 2018. ALL RIGHTS RESERVED. ABN <a href="https://abr.business.gov.au/ABN/View?id=78746032067" target="_blank" rel="noopener">78746032067</a></p>
       <p>WEBSITE BY <a href="https://thinkmilkdesign.co.uk" target="_blank" rel="noopener">TMD</a></p>
     </div>
